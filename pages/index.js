fee56f4e969846568a76631cc3ea065afec8da01
loesen-maurovigliero <mauro.vigliero@loesen.co>
2022-03-31 14:56:29 -0300

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-// import st from '../styles/Home.module.css'
-import Head from 'next/head'
-
+import Head from 'next/head';
 // import Favicons from './../components/Favicon'
 
-import { facebook, instagram, linkedin } from './../components/icon'
+import {
+  iconGithub,
+  iconLinkedIn,
+  iconTwitter,
+  iconFacebook,
+  iconInstagram,
+} from '../components/Icon';
 
 export default function Home() {
   return (
@@ -11,26 +15,25 @@ export default function Home() {
       <Head>
         <title>Mauro Vigliero</title>
       </Head>
-      <div className="w-full h-full justify-center content-center bg-800">
+      <div className='w-full h-full justify-center content-center bg-800'>
         <div>
-          <header className="flex">
-            <img
-              src='/mau.jpg'
-              className="w-12 rounded-full"
-             />
-            <h1 className="text-800">Mauro Vigliero</h1>
+          <header className='flex'>
+            <img src='/mau.jpg' className='w-12 rounded-full' />
+            <h1 className='text-800'>Mauro Vigliero</h1>
           </header>
-          <main className="border-style: solid">
+          <main className='border-style: solid'>
             <div className='p-4 bg-white text-800 flex'>
-              <div className="w-1/4">E-mail</div>
+              <div className='w-1/4'>E-mail</div>
               <div>mauro.vigliero@loesen.co</div>
             </div>
             <div className='p-4 bg-white text-800 flex'>
-              <div className="w-1/4">Profesion</div>
-              <div>Developer <strong> Full-stack </strong> </div>
+              <div className='w-1/4'>Profesion</div>
+              <div>
+                Developer <strong> Full-stack </strong>
+              </div>
             </div>
             <div className='p-4 bg-white text-800 flex'>
-              <div className="w-1/4">Experience</div>
+              <div className='w-1/4'>Experience</div>
               <div>
                 <span>Web disign</span>
                 <span>Front-end</span>
@@ -38,43 +41,51 @@ export default function Home() {
               </div>
             </div>
             <div className='p-4 bg-white text-800 flex'>
-              <div className="w-1/4">Skills</div>
-              <div className="flex flex-wrap">
-                <span className='p-1 bg-800 text-white m-2'>Html - css - javascript</span>
-                <span className='p-1 bg-800 text-white m-2'>Mongodb - mysql</span>
-                <span className='p-1 bg-800 text-white m-2'>Bootstrap - React - Vue</span>            
+              <div className='w-1/4'>Skills</div>
+              <div className='flex flex-wrap'>
+                <span className='p-1 bg-800 text-white m-2'>
+                  Html - css - javascript
+                </span>
+                <span className='p-1 bg-800 text-white m-2'>
+                  Mongodb - mysql
+                </span>
+                <span className='p-1 bg-800 text-white m-2'>
+                  Bootstrap - React - Vue
+                </span>
               </div>
             </div>
             <div className='p-4 bg-white text-800 flex'>
-              <div className="w-1/4">Social</div>
-              <div> 
-              {social.map( data => {
-                <data.icon alt={`Contacto a la red social ${ data.name }`} href={ data.href } />
-              })}
+              <div className='w-1/4'>Social</div>
+              <div>
+                {social.map((data) => {
+                  <data.icon
+                    alt={`Contacto a la red social ${data.name}`}
+                    href={data.href}
+                  />;
+                })}
               </div>
             </div>
           </main>
         </div>
       </div>
-      
     </>
-  )
+  );
 }
 
 const social = [
   {
     name: 'instagram',
     href: 'https://www.instagram.com/maurook789/',
-    icon: instagram,
+    icon: iconInstagram,
   },
   {
     name: 'LinkedIn',
     href: 'https://www.linkedin.com/in/maurovigliero/',
-    icon: linkedin,
+    icon: iconLinkedIn,
   },
   {
     name: 'facebook',
     href: 'https://www.facebook.com/mvigliero/',
-    icon: facebook,
-  }
+    icon: iconFacebook,
+  },
 ];
