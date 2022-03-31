109103bd2af9e25a13d150dd86481891222427ec
loesen-maurovigliero <mauro.vigliero@loesen.co>
2022-03-31 17:01:44 -0300

    Build more style
    Change the colors and adds borders

--- pages/index.js
+++ pages/index.js
@@ -1,70 +1,7 @@
-import Head from 'next/head';
+import Head from 'next/head'
+import Favicons from './../components/Favicons'
 
 import { iconLinkedIn, iconFacebook, iconInstagram } from '../components/Icon';
-
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Mauro Vigliero</title>
-      </Head>
-      <div className='w-full h-full justify-center content-center'>
-        <div>
-          <header className='flex'>
-            <img src='/mau.jpg' className='w-8 rounded-full' />
-            <h1 className='text-800'>Mauro Vigliero</h1>
-          </header>
-          <main className='border-style: solid'>
-            <div className='p-4 bg-white text-800 flex'>
-              <div className='w-1/4'>E-mail</div>
-              <div>mauro.vigliero@loesen.co</div>
-            </div>
-            <div className='p-4 bg-white text-800 flex'>
-              <div className='w-1/4'>Profesion</div>
-              <div>
-                Developer <strong> Full-stack </strong>
-              </div>
-            </div>
-            <div className='p-4 bg-white text-800 flex'>
-              <div className='w-1/4'>Experience</div>
-              <div>
-                <span>Web disign</span>
-                <span>Front-end</span>
-                <span>Back-end</span>
-              </div>
-            </div>
-            <div className='p-4 bg-white text-800 flex'>
-              <div className='w-1/4'>Skills</div>
-              <div className='flex flex-wrap'>
-                <span className='p-1 bg-800 text-white m-2'>
-                  Html - css - javascript
-                </span>
-                <span className='p-1 bg-800 text-white m-2'>
-                  Mongodb - mysql
-                </span>
-                <span className='p-1 bg-800 text-white m-2'>
-                  Bootstrap - React - Vue
-                </span>
-              </div>
-            </div>
-            <div className='p-4 bg-white text-800 flex'>
-              <div className='w-1/4'>Social</div>
-              <div>
-                {social.map((data) => {
-                  <data.icon
-                    alt={`Contacto a la red social ${data.name}`}
-                    href={data.href}
-                  />;
-                })}
-              </div>
-            </div>
-          </main>
-        </div>
-      </div>
-    </>
-  );
-}
-
 const social = [
   {
     name: 'instagram',
@@ -82,3 +19,72 @@ const social = [
     icon: iconFacebook,
   },
 ];
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Mauro Vigliero</title>
+        <Favicons />
+      </Head>
+      <body className='w-full h-screen flex justify-center items-center bg-gray '>
+        <div className='h-max w-1/2 border-t-2 border-l-2 border-r-2 border-white rounded-t-lg'>
+          <header className='flex items-center  py-6'>
+            <div className='mx-4'>
+              <img src='/mau.jpg' className='w-16 rounded-full border-white '/>
+            </div>
+            <h1 className='text-white text-lg'>Mauro Vigliero</h1>
+          </header>
+          <main>
+            <div className='py-6 bg-white flex p-6 border-b-2 border-gray'>
+              <div className='w-1/2'>E-mail</div>
+              <div>mauro.vigliero@loesen.co</div>
+            </div>
+            <div className='py-6 bg-white flex p-6 border-b-2 border-gray'>
+              <div className='w-1/2'>Profesion</div>
+              <div>
+                Developer <strong> Full-stack </strong>
+              </div>
+            </div>
+            <div className='py-6 bg-white flex p-6 border-b-2 border-gray'>
+              <div className='w-1/2 flex items-center'>
+                <span>Experience</span>
+              </div>
+              <div>
+                <p>Web disign</p>
+                <p>Front-end</p>
+                <p>Back-end</p>
+              </div>
+            </div>
+            <div className='py-6 bg-white flex p-6 border-b-2 border-gray'>
+              <div className='w-1/2 flex items-center'>
+                <span>Skills</span>
+              </div>
+              <div className='flex'>
+                <div>
+                  <p>HTML</p>
+                  <p>CSS</p>
+                  <p>Javascript</p>
+                </div>
+                <div className='px-8'>
+                  <p>React</p>
+                  <p>javascript</p>
+                  <p>Bootstrap</p>
+                </div>
+              </div>
+            </div>
+            <div className='bg-white flex p-6'>
+              <div className='w-1/2'>Social</div>
+              <div>
+                {social.map((data) => {
+                  <data.icon />;
+                })}
+              </div>
+            </div>
+          </main>
+        </div>
+      </body>
+    </>
+  );
+}
+
